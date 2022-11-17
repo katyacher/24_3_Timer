e84024f856e154738c487dea1bd960e8ace1b5b8
kate <katyacher@inbox.ru>
2022-11-17 19:45:39 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <ctime>
+#include <chrono>
+#include <thread>
+#include <iomanip>
+
+
+int main() {
+    std::cout << "24.3 Timer" << std::endl;
+
+    std::tm time_tm{};
+    std::cout << "Enter the time (format 00:00): ";
+    std::cin >> std::get_time(&time_tm, "%M:%S");
+
+    if (std::cin.fail()) {
+        std::cout << "Incorrect format. ";
+        return (int) std::cin.fail();
+    }
+
+    std::time_t now_t = time(nullptr);
+    std::tm* stop_tm = std::localtime(&now_t);
+    stop_tm->tm_sec += time_tm.tm_sec;
+    stop_tm->tm_min += time_tm.tm_min;
+
+    std::time_t stop_t = mktime(stop_tm);
+
+    while(now_t != stop_t) {
+        double d = std::difftime(stop_t,now_t);
+        std::time_t diff = (std::time_t)d;
+        std::tm *timer = std::localtime(&diff);
+        std::cout << std::put_time(timer,"%M:%S") << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        now_t = time(nullptr);
+        system("clear");
+    }
+    std::cout << "DING!DING!DING!" << std::endl;
+    return 0;
+}
